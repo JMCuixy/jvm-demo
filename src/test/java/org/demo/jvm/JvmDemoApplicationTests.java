a4b386cf036a99ccda9e9e4d646fe285fb0621ec
cuixiuyin <cuixiuyin@yungu-link.com>
2019-11-12 12:52:52 +0800

    initial commit

---
+++ src/test/java/org/demo/jvm/JvmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
