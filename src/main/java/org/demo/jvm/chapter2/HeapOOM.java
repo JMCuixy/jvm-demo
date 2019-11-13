94bbbdb4bfb34ba3319858ebccb8f181c10b7485
cuixiuyin <1099442418@qq.com>
2019-11-13 22:06:30 +0800

    jvm test

---
+++ src/main/java/org/demo/jvm/chapter2/HeapOOM.java
@@ -0,0 +1,34 @@
+package org.demo.jvm.chapter2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Java 溢出
+ *
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * -Xms20m 设置堆的最小值
+ * -Xmx20m 设置堆的最大值 （-Xms和-Xmx设置成一样可以避免堆自动扩展）
+ * -XX:+HeapDumpOnOutOfMemoryError 出现内存溢出异常时 Dump 出当前的内存堆转储快照
+ *
+ * @author : cuixiuyin
+ * @date : 2019/11/12
+ */
+public class HeapOOM {
+
+
+    static class OOMObject {
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        List<OOMObject> list = new ArrayList<>();
+
+        Thread.sleep(5000L);
+
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
